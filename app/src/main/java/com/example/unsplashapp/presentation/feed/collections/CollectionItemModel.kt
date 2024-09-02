c5f5a356b54c00e4f5b7f01beb95361a61ed9661
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-02 21:20:51 +0700

    create model to display data on UI CollectionItemModel

---
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/CollectionItemModel.kt
@@ -0,0 +1,6 @@
+package com.example.unsplashapp.presentation.feed.collections
+
+// model to display data on UI
+data class CollectionItemModel(
+    val id: String, val title: String, val description: String
+)
