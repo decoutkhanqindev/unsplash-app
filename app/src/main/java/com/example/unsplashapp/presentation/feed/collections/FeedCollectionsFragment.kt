fc77ed78c96bd06e4e700d9ab0099f59560d2173
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-02 22:24:42 +0700

    update all feed fragment include FeedsFragment, FeedCollectionsFragment, FeedPhotosFragment

--- app/src/main/java/com/example/unsplashapp/presentation/feed/collections/FeedCollectionsFragment.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/FeedCollectionsFragment.kt
@@ -2,8 +2,13 @@ package com.example.unsplashapp.presentation.feed.collections
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.unsplashapp.UnsplashServiceLocator
 import com.example.unsplashapp.core.base.BaseFragment
+import com.example.unsplashapp.data.remote.UnsplashApiService
 import com.example.unsplashapp.databinding.FragmentFeedCollectionsBinding
 
 class FeedCollectionsFragment : BaseFragment<FragmentFeedCollectionsBinding>(
@@ -13,11 +18,38 @@ class FeedCollectionsFragment : BaseFragment<FragmentFeedCollectionsBinding>(
         fun newInstance(): FeedCollectionsFragment = FeedCollectionsFragment()
     }
 
-    private val viewModel: CollectionsViewModel by viewModels<CollectionsViewModel>()
+    private val viewModel: CollectionsViewModel by viewModels(factoryProducer = {
+        viewModelFactory {
+            addInitializer(CollectionsViewModel::class) {
+                CollectionsViewModel(unsplashApiService = UnsplashServiceLocator.unsplashApiService)
+            }
+        }
+    })
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel
+        bindViewModel()
+    }
+
+    private fun bindViewModel() {
+        viewModel.uiState.observe(viewLifecycleOwner) { uiState: CollectionsUiState ->
+            when (uiState) {
+                CollectionsUiState.FirstPageLoading -> { // -> show loading
+                    binding.progressCircular.isVisible = true
+                    binding.buttonRetry.isVisible = false
+                }
+
+                CollectionsUiState.FirstPageError -> {  // -> show error
+                    binding.progressCircular.isVisible = false
+                    binding.buttonRetry.isVisible = true
+                }
+
+                is CollectionsUiState.Content -> { // -> show content
+                    binding.progressCircular.isVisible = false
+                    binding.buttonRetry.isVisible = false
+                }
+            }
+        }
     }
 }
