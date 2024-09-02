5d4ad3bd0abaa0634dc82e40967c2961972c997c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-02 17:23:34 +0700

    update interface to add endpoint api UnsplashApiService

--- app/src/main/java/com/example/unsplashapp/data/remote/UnsplashApiService.kt
+++ app/src/main/java/com/example/unsplashapp/data/remote/UnsplashApiService.kt
@@ -1,10 +1,18 @@
 package com.example.unsplashapp.data.remote
 
+import com.example.unsplashapp.data.remote.response.CollectionItemResponse
 import retrofit2.Retrofit
 import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface UnsplashApiService {
     companion object {
         operator fun invoke(retrofit: Retrofit): UnsplashApiService = retrofit.create()
     }
+
+    @GET("collections")
+    suspend fun getCollections(
+        @Query("page") page: Int, @Query("per_page") perPage: Int
+    ): List<CollectionItemResponse>
 }
