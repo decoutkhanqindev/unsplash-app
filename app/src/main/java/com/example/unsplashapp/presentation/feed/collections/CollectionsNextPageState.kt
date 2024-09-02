9343e672329e86f1c3c5273e208fa2b98c429384
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-02 21:29:37 +0700

    update enum class to represent for last elements -> loading or error CollectionsNextPageState

--- app/src/main/java/com/example/unsplashapp/presentation/feed/collections/CollectionsNextPageState.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/CollectionsNextPageState.kt
@@ -4,6 +4,6 @@ package com.example.unsplashapp.presentation.feed.collections
 enum class CollectionsNextPageState {
     LOADING, // -> load more a next page state is loading
     ERROR, // -> load more a next page state is error
-    IDLE, // -> load more a next page state is normal
-    DONE, // -> load more a next page state is done -> it scrolled a last element -> done loading all elements
+    IDLE, // -> load more a next page state is normal -> no loading and no error
+    NO_MORE_ITEMS, // -> load more a next page state is done -> it scrolled a last element -> done loading all elements
 }
