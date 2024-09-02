d393677ba71fadc8d874df60a8c783a9c872a3d0
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-02 15:18:37 +0700

    add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ dependencies {
 
     // OkHttp
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
-    implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.okhttp3:logging-interceptor")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
 
     // Gson
     implementation("com.google.code.gson:gson:2.11.0")
