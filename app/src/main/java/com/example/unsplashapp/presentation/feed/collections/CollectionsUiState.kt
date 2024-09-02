82cc13b6c84264d53bec8213f0d2b279587758bf
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-02 21:30:04 +0700

    update sealed interface to handle display data on ui CollectionsUiState

--- app/src/main/java/com/example/unsplashapp/presentation/feed/collections/CollectionsUiState.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/CollectionsUiState.kt
@@ -1,9 +1,11 @@
 package com.example.unsplashapp.presentation.feed.collections
 
-import com.example.unsplashapp.data.remote.response.CollectionItemResponse
-
 sealed interface CollectionsUiState {
     data object FirstPageLoading: CollectionsUiState
     data object FirstPageError: CollectionsUiState
-    data class Content(val items: List<CollectionItemModel>): CollectionsUiState
+    data class Content(
+        val items: List<CollectionItemModel>,
+        val currentPage: Int,
+        val nextPageState: CollectionsNextPageState
+    ): CollectionsUiState
 }
