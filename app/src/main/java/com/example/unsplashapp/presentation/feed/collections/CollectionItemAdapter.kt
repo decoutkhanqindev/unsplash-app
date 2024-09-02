66c8e8de2559e3d79c601c7f3f04c2434d85998b
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-02 22:55:15 +0700

    create feed collection adapter to display data CollectionItemAdapter

--- app/src/main/java/com/example/unsplashapp/presentation/feed/collections/CollectionItemAdapter.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/CollectionItemAdapter.kt
@@ -1,30 +1,48 @@
 package com.example.unsplashapp.presentation.feed.collections
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.example.unsplashapp.databinding.ItemCollectionUiModelBinding
 
-class CollectionItemAdapter(diffCallback: CollectionItemCallBack) :
-    ListAdapter<CollectionItemModel, CollectionItemAdapter.VH>(diffCallback) {
-    private val items: List<CollectionItemModel> = emptyList()
+class CollectionItemAdapter(
+    private val requestManager: RequestManager
+) : ListAdapter<CollectionItemModel, CollectionItemAdapter.VH>(CollectionItemCallBack) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH = VH(
+        ItemCollectionUiModelBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    )
+
+    override fun onBindViewHolder(holder: VH, position: Int): Unit = holder.bind(getItem(position))
 
     inner class VH(
         private val binding: ItemCollectionUiModelBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind() {
+        fun bind(item: CollectionItemModel) {
+            binding.run {
+                requestManager.load(item.coverPhotoUrl).fitCenter().centerCrop()
+                    .transition(withCrossFade()).into(imageCollection)
 
+                textTitle.text = item.title
+                textDescription.text = item.description
+            }
         }
     }
+}
 
-    private object CollectionItemCallBack : ItemCallback<CollectionItemModel>() {
-        override fun areContentsTheSame(
-            oldItem: CollectionItemModel, newItem: CollectionItemModel
-        ): Boolean = oldItem == newItem
+private object CollectionItemCallBack : ItemCallback<CollectionItemModel>() {
+    override fun areItemsTheSame(
+        oldItem: CollectionItemModel, newItem: CollectionItemModel
+    ): Boolean = oldItem.id == newItem.id
 
-        override fun areItemsTheSame(
-            oldItem: CollectionItemModel, newItem: CollectionItemModel
-        ): Boolean = oldItem.id == newItem.id
-    }
+    override fun areContentsTheSame(
+        oldItem: CollectionItemModel, newItem: CollectionItemModel
+    ): Boolean = oldItem == newItem
 }
