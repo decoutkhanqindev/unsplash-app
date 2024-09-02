3a0c3733f740a2c3ce9fa3c625af5a1f90173197
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-02 16:53:31 +0700

    handle display FeedsFragment in MainActivity

--- app/src/main/java/com/example/unsplashapp/MainActivity.kt
+++ app/src/main/java/com/example/unsplashapp/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.unsplashapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.add
+import com.example.unsplashapp.databinding.ActivityMainBinding
+import com.example.unsplashapp.presentation.feed.FeedsFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                addToBackStack(null)
+                add<FeedsFragment>(
+                    containerViewId = R.id.main, tag = FeedsFragment::class.simpleName
+                )
+            }
+        }
     }
 }
