5d63e02e99334c6fdf1ae3649e754bfe04b41a81
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-03 16:48:24 +0700

    update all feed fragment include FeedsFragment, FeedCollectionsFragment, FeedPhotosFragment, FeedCollectionItemFragment

---
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/FeedCollectionItemFragment.kt
@@ -0,0 +1,12 @@
+package com.example.unsplashapp.presentation.feed.collections
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.unsplashapp.R
+
+class FeedCollectionItemFragment : Fragment() {
+
+}
