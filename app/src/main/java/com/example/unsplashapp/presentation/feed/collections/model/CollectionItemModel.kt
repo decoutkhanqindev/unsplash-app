0be1b79e41714891b44ea9a49c69d6e585a312a5
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-03 16:27:03 +0700

    update model to display data on UI CollectionItemModel

--- app/src/main/java/com/example/unsplashapp/presentation/feed/collections/model/CollectionItemModel.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/model/CollectionItemModel.kt
@@ -1,6 +1,12 @@
 package com.example.unsplashapp.presentation.feed.collections.model
 
+import com.example.unsplashapp.data.remote.response.CollectionItemResponse.PreviewPhoto
+
 // model to display data on UI
 data class CollectionItemModel(
-    val id: String, val title: String, val description: String, val coverPhotoUrl: String
+    val id: String,
+    val title: String,
+    val description: String,
+    val coverPhotoUrl: String,
+    val previewPhotos: List<PreviewPhoto>
 )
