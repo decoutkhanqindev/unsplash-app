68cbaf31daf3825c493ee2c9cad00629d744a3e9
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-04 15:58:37 +0700

    create model to display data on UI PhotoItemModel

--- app/src/main/java/com/example/unsplashapp/presentation/feed/photos/model/PhotoItemModel.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/photos/model/PhotoItemModel.kt
@@ -3,5 +3,5 @@ package com.example.unsplashapp.presentation.feed.photos.model
 import com.example.unsplashapp.data.remote.response.PhotoItemResponse
 
 data class PhotoItemModel(
-    val id: String, val description: String, val urls: PhotoItemResponse.Urls
+    val id: String, val description: String?, val urls: PhotoItemResponse.Urls
 )
