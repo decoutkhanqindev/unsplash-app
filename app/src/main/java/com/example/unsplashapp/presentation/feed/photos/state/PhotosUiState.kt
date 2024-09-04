01704b48a996ad042ae4fdc24a8c3978eebfe030
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-04 15:53:17 +0700

    update sealed interface to handle display data on ui PhotosUiState

--- app/src/main/java/com/example/unsplashapp/presentation/feed/photos/state/PhotosUiState.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/photos/state/PhotosUiState.kt
@@ -1,12 +1,12 @@
 package com.example.unsplashapp.presentation.feed.photos.state
 
-import com.example.unsplashapp.data.remote.response.PhotoItemResponse
+import com.example.unsplashapp.presentation.feed.photos.model.PhotoItemModel
 
 sealed interface PhotosUiState {
     data object FirstPageLoading : PhotosUiState
     data object FirstPageError : PhotosUiState
     data class Content(
-        val items: List<PhotoItemResponse>,
+        val items: List<PhotoItemModel>,
         val currentPage: Int,
         val nextPageState: PhotosNextPageState
     ) : PhotosUiState
