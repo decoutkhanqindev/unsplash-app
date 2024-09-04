fc94b8e8c6cbf90a880f47304406c20c47001fb0
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-04 17:34:52 +0700

    update all feed fragment include FeedsFragment, FeedCollectionsFragment, FeedPhotosFragment, FeedCollectionItemPreviewPhotoFragment

--- app/src/main/java/com/example/unsplashapp/presentation/feed/photos/FeedPhotosFragment.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/photos/FeedPhotosFragment.kt
@@ -6,6 +6,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.unsplashapp.UnsplashServiceLocator
 import com.example.unsplashapp.core.base.BaseFragment
@@ -18,6 +19,7 @@ class FeedPhotosFragment : BaseFragment<FragmentFeedPhotosBinding>(
 ) {
     companion object {
         fun newInstance(): FeedPhotosFragment = FeedPhotosFragment()
+        private const val VISIBLE_THRESHOLD = 2 // -> 2 items is visible
     }
 
     private val viewModel: PhotosViewModel by viewModels<PhotosViewModel>(factoryProducer = {
@@ -69,6 +71,19 @@ class FeedPhotosFragment : BaseFragment<FragmentFeedPhotosBinding>(
                 }
             }
         }
+
+        // config to load next page
+        val linearLayoutManager: LinearLayoutManager =
+            binding.photosRecyclerView.layoutManager as LinearLayoutManager
+        binding.photosRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            // OnScrollListener designed to detect when the user scrolls near the bottom of the RecyclerView and potentially trigger an action like loading more data
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if ((dy > 0) && ((linearLayoutManager.findLastVisibleItemPosition() + VISIBLE_THRESHOLD) >= linearLayoutManager.itemCount)) {
+                    // If there are 2 more elements from the last element, you can call loadNextPage()
+                    viewModel.loadNextPage()
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
