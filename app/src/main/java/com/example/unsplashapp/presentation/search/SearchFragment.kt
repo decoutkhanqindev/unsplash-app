6f120ff30f416185987805faa737fa8fe9d7ee7f
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-06 13:00:00 +0700

    update all search fragment include SearchFragment, SearchPhotosFragment, SearchUsersFragment

--- app/src/main/java/com/example/unsplashapp/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/search/SearchFragment.kt
@@ -56,11 +56,9 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(
     private fun setUpTextChanges() {
         binding.searchTextInputEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
             }
 
             override fun afterTextChanged(s: Editable?) {
