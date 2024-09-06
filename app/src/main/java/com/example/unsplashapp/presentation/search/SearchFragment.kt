6ada680ad60075773c6350803f4ec4b659136335
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-06 14:28:01 +0700

    update all search fragment include SearchFragment, SearchPhotosFragment, SearchUsersFragment

--- app/src/main/java/com/example/unsplashapp/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/search/SearchFragment.kt
@@ -3,6 +3,7 @@ package com.example.unsplashapp.presentation.search
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewmodel.viewModelFactory
