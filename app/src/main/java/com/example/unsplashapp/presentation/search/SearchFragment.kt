0be7e1dd26af70175fa92c0b932888c27a37acd3
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-06 11:18:37 +0700

    update all search fragment include SearchFragment, SearchPhotosFragment, SearchUsersFragment

--- app/src/main/java/com/example/unsplashapp/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/search/SearchFragment.kt
@@ -1,13 +1,6 @@
 package com.example.unsplashapp.presentation.search
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.unsplashapp.R
 import com.example.unsplashapp.core.base.BaseFragment
-import com.example.unsplashapp.databinding.FragmentFeedsBinding
 import com.example.unsplashapp.databinding.FragmentSearchBinding
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(
