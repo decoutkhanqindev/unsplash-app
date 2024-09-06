5b21a0ef5da7c6aee3b5541633a2c0e102dd3534
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-06 13:36:04 +0700

    update all search fragment include SearchFragment, SearchPhotosFragment, SearchUsersFragment

--- app/src/main/java/com/example/unsplashapp/presentation/search/SearchViewPagerAdapter.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/search/SearchViewPagerAdapter.kt
@@ -2,9 +2,8 @@ package com.example.unsplashapp.presentation.search
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.unsplashapp.presentation.feed.collections.FeedCollectionsFragment
-import com.example.unsplashapp.presentation.feed.photos.FeedPhotosFragment
 import com.example.unsplashapp.presentation.search.photos.SearchPhotosFragment
+import com.example.unsplashapp.presentation.search.users.SearchUsersFragment
 
 class SearchViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int = 2
@@ -12,7 +11,7 @@ class SearchViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment
     override fun createFragment(position: Int): Fragment = // -> always return a new obj fragment
         when (position) {
             0 -> SearchPhotosFragment.newInstance()
-            1 -> FeedPhotosFragment.newInstance()
+            1 -> SearchUsersFragment.newInstance()
             else -> throw IllegalArgumentException("Invalid position: $position")
         }
 }
