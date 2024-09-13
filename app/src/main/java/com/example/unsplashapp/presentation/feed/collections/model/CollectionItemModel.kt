cb10b2b490462fedd5abe86655e152f4ca43368c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-14 01:08:49 +0700

    update model to display data on UI CollectionItemModel

--- app/src/main/java/com/example/unsplashapp/presentation/feed/collections/model/CollectionItemModel.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/model/CollectionItemModel.kt
@@ -1,25 +1,60 @@
 package com.example.unsplashapp.presentation.feed.collections.model
 
+import android.annotation.SuppressLint
+import android.os.Parcel
+import android.os.Parcelable
 import com.example.unsplashapp.data.remote.response.CollectionItemResponse
-import java.io.Serializable
 
 // model to display data on UI for collection
+@SuppressLint("ParcelCreator")
+@Suppress("DEPRECATION")
 data class CollectionItemModel(
-	val id: String,
-	val title: String,
-	val description: String,
-	val coverPhotoUrl: String,
-	val user: CollectionItemResponse.User,
-	val previewPhotos: List<CollectionItemResponse.PreviewPhoto>
-) : Serializable {
-	companion object {
-		fun CollectionItemResponse.toCollectionItemModel(): CollectionItemModel = CollectionItemModel(
-			id = id,
-			title = title,
-			description = description ?: "No description...",
-			coverPhotoUrl = coverPhoto.urls.regular,
-			user = user,
-			previewPhotos = previewPhotos
-		)
-	}
+  val id: String,
+  val title: String,
+  val description: String,
+  val coverPhotoUrl: String,
+  val user: CollectionItemResponse.User,
+  val previewPhotos: List<CollectionItemResponse.PreviewPhoto>
+) : Parcelable {
+  
+  constructor(parcel: Parcel) : this(
+    parcel.readString()!!,
+    parcel.readString()!!,
+    parcel.readString()!!,
+    parcel.readString()!!,
+    parcel.readParcelable(CollectionItemResponse.User::class.java.classLoader)!!,
+    parcel.createTypedArrayList(CollectionItemResponse.PreviewPhoto.CREATOR)!!
+  )
+  
+  override fun writeToParcel(parcel: Parcel, flags: Int) {
+    parcel.writeString(id)
+    parcel.writeString(title)
+    parcel.writeString(description)
+    parcel.writeString(coverPhotoUrl)
+    parcel.writeParcelable(user, flags)
+    parcel.writeTypedList(previewPhotos)
+  }
+  
+  override fun describeContents(): Int {
+    return 0
+  }
+  
+  companion object CREATOR : Parcelable.Creator<CollectionItemModel> {
+    override fun createFromParcel(parcel: Parcel): CollectionItemModel {
+      return CollectionItemModel(parcel)
+    }
+    
+    override fun newArray(size: Int): Array<CollectionItemModel?> {
+      return arrayOfNulls(size)
+    }
+    
+    fun CollectionItemResponse.toCollectionItemModel(): CollectionItemModel = CollectionItemModel(
+      id = id,
+      title = title,
+      description = description,
+      coverPhotoUrl = coverPhoto.urls.regular,
+      user = user,
+      previewPhotos = previewPhotos
+    )
+  }
 }
