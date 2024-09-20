0feb487bf3b95940481ce3c0244dd8c73ad1fd11
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-21 00:06:27 +0700

    update feed collection adapter to display data CollectionItemAdapter

--- app/src/main/java/com/example/unsplashapp/presentation/feed/collections/adapter/CollectionItemAdapter.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/feed/collections/adapter/CollectionItemAdapter.kt
@@ -2,6 +2,7 @@ package com.example.unsplashapp.presentation.feed.collections.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
@@ -44,5 +45,15 @@ class CollectionItemAdapter(
         collectionItemDescription.text = item.description ?: "No description ..."
       }
     }
+  }
+  
+  private object CollectionItemCallBack : ItemCallback<CollectionItemModel>() {
+    override fun areItemsTheSame(
+      oldItem: CollectionItemModel, newItem: CollectionItemModel
+    ): Boolean = oldItem.id == newItem.id
+    
+    override fun areContentsTheSame(
+      oldItem: CollectionItemModel, newItem: CollectionItemModel
+    ): Boolean = oldItem == newItem
   }
 }
