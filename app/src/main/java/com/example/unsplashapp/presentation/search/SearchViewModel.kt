32cc26e086fefd259aac274e8bb73022364f0d20
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-21 01:32:16 +0700

    update

--- app/src/main/java/com/example/unsplashapp/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/search/SearchViewModel.kt
@@ -9,8 +9,6 @@ import androidx.lifecycle.liveData
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import com.example.unsplashapp.data.remote.UnsplashApiService
-import com.example.unsplashapp.data.remote.response.SearchPhotosResponse
-import com.example.unsplashapp.data.remote.response.SearchUsersResponse
 import com.example.unsplashapp.presentation.feed.photos.model.PhotoItemModel
 import com.example.unsplashapp.presentation.feed.photos.model.PhotoItemModel.Companion.toPhotoItemModel
 import com.example.unsplashapp.presentation.search.users.model.UserItemModel
@@ -24,29 +22,17 @@ class SearchViewModel(private val unsplashApiService: UnsplashApiService) : View
   private val _searchQuery: MutableLiveData<String> = MutableLiveData<String>("")
   private val searchQuery: LiveData<String> get() = _searchQuery
   
-  internal val searchPhotosLiveData: LiveData<List<PhotoItemModel>> = searchQuery.debounce(
-    650L, viewModelScope
-  ) // -> avoid multiple requests, after 650L request starts
-    .distinctUntilChanged() // -> if next value changes but equals with previous value -> not request
-    .switchMap { query: String ->
-      if (query.isNotBlank()) {
-        liveData(context = viewModelScope.coroutineContext + Dispatchers.IO) {
-          val modelItems: List<PhotoItemModel> = searchItems(query)
-          emit(modelItems)
-        }
-      } else {
-        liveData { emit(emptyList()) }
-      }
-    }
+  val searchPhotosLiveData: LiveData<List<PhotoItemModel>> =
+    generateSearchItemsLiveData<PhotoItemModel>()
   
-  internal val searchUsersLiveData: LiveData<List<UserItemModel>> = searchQuery.debounce(
-    650L, viewModelScope
-  ) // -> avoid multiple requests, after 650L request starts
-    .distinctUntilChanged() // -> if next value changes but equals with previous value -> not request
-    .switchMap { query: String ->
+  val searchUsersLiveData: LiveData<List<UserItemModel>> =
+    generateSearchItemsLiveData<UserItemModel>()
+  
+  private inline fun <reified T> generateSearchItemsLiveData(): LiveData<List<T>> =
+    searchQuery.debounce(650L, viewModelScope).distinctUntilChanged().switchMap { query: String ->
       if (query.isNotBlank()) {
         liveData(context = viewModelScope.coroutineContext + Dispatchers.IO) {
-          val modelItems: List<UserItemModel> = searchItems(query)
+          val modelItems: List<T> = searchItems<T>(query)
           emit(modelItems)
         }
       } else {
@@ -78,34 +64,6 @@ class SearchViewModel(private val unsplashApiService: UnsplashApiService) : View
       throw cancel
     } catch (e: Exception) {
       Log.d("SearchViewModel", "searchItems: ${e.message}")
-      emptyList()
-    }
-  }
-  
-  private suspend fun searchPhotos(query: String): List<PhotoItemModel> {
-    return try {
-      val responseItem: SearchPhotosResponse = unsplashApiService.searchPhotos(
-        query = query, page = 1, perPage = 30
-      )
-      responseItem.results.map { it.toPhotoItemModel() }
-    } catch (cancel: CancellationException) {
-      throw cancel
-    } catch (e: Exception) {
-      Log.d("SearchViewModel", "searchPhotos: ${e.message}")
-      emptyList()
-    }
-  }
-  
-  private suspend fun searchUsers(query: String): List<UserItemModel> {
-    return try {
-      val responseItem: SearchUsersResponse = unsplashApiService.searchUsers(
-        query = query, page = 1, perPage = 30
-      )
-      responseItem.results.map { it.toUserItemModel() }
-    } catch (cancel: CancellationException) {
-      throw cancel
-    } catch (e: Exception) {
-      Log.d("SearchViewModel", "searchUsers: ${e.message}")
       emptyList()
     }
   }
