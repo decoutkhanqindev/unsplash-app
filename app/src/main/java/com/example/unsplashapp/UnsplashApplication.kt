06591e9d63ecacfa1ee9be44c81635d0e9c5399e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-25 16:22:05 +0700

    update

--- app/src/main/java/com/example/unsplashapp/UnsplashApplication.kt
+++ app/src/main/java/com/example/unsplashapp/UnsplashApplication.kt
@@ -1,6 +1,7 @@
 package com.example.unsplashapp
 
 import android.app.Application
+import com.example.unsplashapp.di.UnsplashServiceLocator
 
 class UnsplashApplication : Application() {
   override fun onCreate() {
--- app/src/main/java/com/example/unsplashapp/glide/UnsplashGlideModule.kt
+++ app/src/main/java/com/example/unsplashapp/glide/UnsplashGlideModule.kt
@@ -7,7 +7,7 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
-import com.example.unsplashapp.UnsplashServiceLocator
+import com.example.unsplashapp.di.UnsplashServiceLocator
 import java.io.InputStream
 
 @GlideModule
