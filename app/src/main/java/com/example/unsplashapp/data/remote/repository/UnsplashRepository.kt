8d1aa57e619fb98560fb5f4f9e62affbde3f0dee
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-25 16:22:23 +0700

    update UnsplashRepository

--- app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepository.kt
+++ app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepository.kt
@@ -1,4 +1,17 @@
 package com.example.unsplashapp.data.remote.repository
 
-class UnsplashRepository {
+import com.example.unsplashapp.data.remote.response.CollectionItemResponse
+import com.example.unsplashapp.data.remote.response.PhotoItemResponse
+import com.example.unsplashapp.data.remote.response.SearchPhotosResponse
+import com.example.unsplashapp.data.remote.response.SearchUsersResponse
+
+// abstractions data source
+interface UnsplashRepository {
+  suspend fun getCollections(page: Int, perPage: Int): List<CollectionItemResponse>
+  
+  suspend fun getPhotos(page: Int, perPage: Int): List<PhotoItemResponse>
+  
+  suspend fun searchPhotos(query: String, page: Int, perPage: Int): SearchPhotosResponse
+  
+  suspend fun searchUsers(query: String, page: Int, perPage: Int): SearchUsersResponse
 }
--- app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepositoryImpl.kt
+++ app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepositoryImpl.kt
@@ -1,4 +1,23 @@
 package com.example.unsplashapp.data.remote.repository
 
-class UnsplashRepositoryImpl {
+import com.example.unsplashapp.data.remote.UnsplashApiService
+import com.example.unsplashapp.data.remote.response.CollectionItemResponse
+import com.example.unsplashapp.data.remote.response.PhotoItemResponse
+import com.example.unsplashapp.data.remote.response.SearchPhotosResponse
+import com.example.unsplashapp.data.remote.response.SearchUsersResponse
+
+class UnsplashRepositoryImpl(
+  private val unsplashApiService: UnsplashApiService
+) : UnsplashRepository {
+  override suspend fun getCollections(page: Int, perPage: Int): List<CollectionItemResponse> =
+    unsplashApiService.getCollections(page, perPage)
+  
+  override suspend fun getPhotos(page: Int, perPage: Int): List<PhotoItemResponse> =
+    unsplashApiService.getPhotos(page, perPage)
+  
+  override suspend fun searchPhotos(query: String, page: Int, perPage: Int): SearchPhotosResponse =
+    unsplashApiService.searchPhotos(query, page, perPage)
+  
+  override suspend fun searchUsers(query: String, page: Int, perPage: Int): SearchUsersResponse =
+    unsplashApiService.searchUsers(query, page, perPage)
 }
