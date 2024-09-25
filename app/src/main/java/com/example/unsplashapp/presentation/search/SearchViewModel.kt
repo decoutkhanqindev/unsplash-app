47194b363fcd6f12df6512d50bcda9dac08e2c35
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-25 17:46:10 +0700

    update view model to display data on UI FeedsViewModel

--- app/src/main/java/com/example/unsplashapp/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/search/SearchViewModel.kt
@@ -8,7 +8,7 @@ import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.liveData
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
-import com.example.unsplashapp.data.remote.UnsplashApiService
+import com.example.unsplashapp.data.remote.repository.UnsplashRepository
 import com.example.unsplashapp.presentation.feed.photos.model.PhotoItemModel
 import com.example.unsplashapp.presentation.feed.photos.model.PhotoItemModel.Companion.toPhotoItemModel
 import com.example.unsplashapp.presentation.search.users.model.UserItemModel
@@ -17,14 +17,14 @@ import com.example.unsplashapp.presentation.search.utils.Debounce.debounce
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 
-class SearchViewModel(private val unsplashApiService: UnsplashApiService) : ViewModel() {
+class SearchViewModel(private val repository: UnsplashRepository) : ViewModel() {
   private val _searchQuery: MutableLiveData<String> = MutableLiveData<String>("")
   private val searchQuery: LiveData<String> get() = _searchQuery
   
-  val searchPhotosLiveData: LiveData<List<PhotoItemModel>> =
+  internal val searchPhotosLiveData: LiveData<List<PhotoItemModel>> =
     generateSearchItemsLiveData<PhotoItemModel>()
   
-  val searchUsersLiveData: LiveData<List<UserItemModel>> =
+  internal val searchUsersLiveData: LiveData<List<UserItemModel>> =
     generateSearchItemsLiveData<UserItemModel>()
   
   // reified is check type at runtime.
@@ -42,32 +42,22 @@ class SearchViewModel(private val unsplashApiService: UnsplashApiService) : View
   
   @Suppress("UNCHECKED_CAST")
   // reified is check type at at runtime.
-  private suspend inline fun <reified T> searchItems(query: String): List<T> {
-    return try {
-      val responseItems: List<T> = when (T::class) {
-        PhotoItemModel::class -> {
-          unsplashApiService.searchPhotos(
-            query, 1, 30
-          ).results.map { it.toPhotoItemModel() } as List<T>
-        }
-        
-        UserItemModel::class -> {
-          unsplashApiService.searchUsers(
-            query, 1, 30
-          ).results.map { it.toUserItemModel() } as List<T>
-        }
-        
-        else -> {
-          throw IllegalArgumentException("Unsupported type")
-        }
-      }
-      responseItems
-    } catch (cancel: CancellationException) {
-      throw cancel
-    } catch (e: Exception) {
-      Log.d("SearchViewModel", "searchItems: ${e.message}")
-      emptyList()
+  private suspend inline fun <reified T> searchItems(query: String): List<T> = try {
+    val responseItems: List<T> = when (T::class) {
+      PhotoItemModel::class ->
+        repository.searchPhotos(query, 1, 30).results.map { it.toPhotoItemModel() } as List<T>
+      
+      UserItemModel::class ->
+        repository.searchUsers(query, 1, 30).results.map { it.toUserItemModel() } as List<T>
+      
+      else -> throw IllegalArgumentException("Unsupported type")
     }
+    responseItems
+  } catch (cancel: CancellationException) {
+    throw cancel
+  } catch (e: Exception) {
+    Log.d("SearchViewModel", "searchItems: ${e.message}")
+    emptyList()
   }
   
   fun setQuery(query: String) {
