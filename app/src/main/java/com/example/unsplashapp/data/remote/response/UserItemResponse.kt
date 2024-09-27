505de0463d29e683dfa160c56384405dbfa4cd35
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-28 03:52:17 +0700

    update

--- app/src/main/java/com/example/unsplashapp/data/remote/response/UserItemResponse.kt
+++ app/src/main/java/com/example/unsplashapp/data/remote/response/UserItemResponse.kt
@@ -2,7 +2,6 @@ package com.example.unsplashapp.data.remote.response
 
 import androidx.annotation.Keep
 import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
 @Keep
 data class UserItemResponse(
