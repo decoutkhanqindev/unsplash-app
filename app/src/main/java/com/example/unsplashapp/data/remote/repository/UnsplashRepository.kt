623ce3abde75bcd9d6c70cf0d858e580c3a4a2e0
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-28 03:50:47 +0700

    update UnsplashRepository

--- app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepository.kt
+++ app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepository.kt
@@ -11,12 +11,12 @@ interface UnsplashRepository {
   suspend fun getCollections(page: Int, perPage: Int): List<CollectionItemResponse>
   
   suspend fun getPhotos(page: Int, perPage: Int): List<PhotoItemResponse>
-  
+
 //  suspend fun searchPhotos(query: String, page: Int, perPage: Int): SearchPhotosResponse
 
 //  suspend fun searchUsers(query: String, page: Int, perPage: Int): SearchUsersResponse
   
-  suspend fun searchPhotosByRxJava(query: String, page: Int, perPage: Int): Single<SearchPhotosResponse>
+  fun searchPhotosByRxJava(query: String, page: Int, perPage: Int): Single<SearchPhotosResponse>
   
-  suspend fun searchUsersByRxJava(query: String, page: Int, perPage: Int): Single<SearchUsersResponse>
+  fun searchUsersByRxJava(query: String, page: Int, perPage: Int): Single<SearchUsersResponse>
 }
