f0062aa66422b77ecb9b2fcf2c61b12917379c5f
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-28 00:18:55 +0700

    update UnsplashRepository

--- app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepository.kt
+++ app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepository.kt
@@ -4,6 +4,7 @@ import com.example.unsplashapp.data.remote.response.CollectionItemResponse
 import com.example.unsplashapp.data.remote.response.PhotoItemResponse
 import com.example.unsplashapp.data.remote.response.SearchPhotosResponse
 import com.example.unsplashapp.data.remote.response.SearchUsersResponse
+import io.reactivex.rxjava3.core.Single
 
 // abstractions data source
 interface UnsplashRepository {
@@ -13,5 +14,9 @@ interface UnsplashRepository {
   
   suspend fun searchPhotos(query: String, page: Int, perPage: Int): SearchPhotosResponse
   
+  suspend fun searchPhotosByRxJava(query: String, page: Int, perPage: Int): Single<SearchPhotosResponse>
+  
   suspend fun searchUsers(query: String, page: Int, perPage: Int): SearchUsersResponse
+  
+  suspend fun searchUsersByRxJava(query: String, page: Int, perPage: Int): Single<SearchUsersResponse>
 }
--- app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepositoryImpl.kt
+++ app/src/main/java/com/example/unsplashapp/data/remote/repository/UnsplashRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.example.unsplashapp.data.remote.response.CollectionItemResponse
 import com.example.unsplashapp.data.remote.response.PhotoItemResponse
 import com.example.unsplashapp.data.remote.response.SearchPhotosResponse
 import com.example.unsplashapp.data.remote.response.SearchUsersResponse
+import io.reactivex.rxjava3.core.Single
 
 class UnsplashRepositoryImpl(
   private val unsplashApiService: UnsplashApiService
@@ -18,6 +19,14 @@ class UnsplashRepositoryImpl(
   override suspend fun searchPhotos(query: String, page: Int, perPage: Int): SearchPhotosResponse =
     unsplashApiService.searchPhotos(query, page, perPage)
   
+  override suspend fun searchPhotosByRxJava(
+    query: String, page: Int, perPage: Int
+  ): Single<SearchPhotosResponse> = unsplashApiService.searchPhotosByRxJava(query, page, perPage)
+  
   override suspend fun searchUsers(query: String, page: Int, perPage: Int): SearchUsersResponse =
     unsplashApiService.searchUsers(query, page, perPage)
+  
+  override suspend fun searchUsersByRxJava(
+    query: String, page: Int, perPage: Int
+  ): Single<SearchUsersResponse> = unsplashApiService.searchUsersByRxJava(query, page, perPage)
 }
