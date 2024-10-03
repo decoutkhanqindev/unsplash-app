03322ebe9cb540483eb282ece5aa99b29d574b97
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-03 23:21:13 +0700

    update MainActivity

--- app/src/main/java/com/example/unsplashapp/MainActivity.kt
+++ app/src/main/java/com/example/unsplashapp/MainActivity.kt
@@ -6,7 +6,24 @@ import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.unsplashapp.databinding.ActivityMainBinding
 import com.example.unsplashapp.presentation.feed.FeedsFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+/*
+@AndroidEntryPoint is a Hilt annotation that you can use on Android classes
+that need to receive dependencies from Hilt.
+Here's how it works:
+Tells Hilt to provide dependencies: When you annotate an Android class with @AndroidEntryPoint,
+you're telling Hilt that this class needs to participate in dependency injection. Hilt will generate
+a dependency container (a component) for that class and provide dependencies to it.
+Supported classes: You can use @AndroidEntryPoint on a variety of Android classes, including:
+- Activity
+- Fragment
+- View
+- Service
+- BroadcastReceiver
+*/
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
   private val binding: ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
     ActivityMainBinding.inflate(layoutInflater)
