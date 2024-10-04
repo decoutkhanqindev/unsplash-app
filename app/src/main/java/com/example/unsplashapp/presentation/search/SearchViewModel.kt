cf462b5ba2f222c7998867f8d56b4c0d08d764b9
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-05 01:01:50 +0700

    update view model to display data on UI SearchViewModel

--- app/src/main/java/com/example/unsplashapp/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/example/unsplashapp/presentation/search/SearchViewModel.kt
@@ -3,18 +3,13 @@ package com.example.unsplashapp.presentation.search
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.unsplashapp.data.remote.repository.UnsplashRepository
-import com.example.unsplashapp.data.remote.response.PhotoItemResponse
-import com.example.unsplashapp.data.remote.response.SearchPhotosResponse
-import com.example.unsplashapp.data.remote.response.SearchUsersResponse
-import com.example.unsplashapp.data.remote.response.UserItemResponse
 import com.example.unsplashapp.presentation.feed.photos.model.PhotoItemModel
-import com.example.unsplashapp.presentation.feed.photos.model.PhotoItemModel.Companion.toPhotoItemModel
 import com.example.unsplashapp.presentation.search.users.model.UserItemModel
-import com.example.unsplashapp.presentation.search.users.model.UserItemModel.Companion.toUserItemModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.BehaviorSubject
@@ -22,7 +17,9 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor(private val repository: UnsplashRepository) : ViewModel() {
+class SearchViewModel @Inject constructor(private val repository: UnsplashRepository) :
+  ViewModel() {
+
 //  private val _searchQuery: MutableLiveData<String> = MutableLiveData<String>("")
 //  private val searchQuery: LiveData<String> get() = _searchQuery
 //
@@ -31,30 +28,6 @@ class SearchViewModel @Inject constructor(private val repository: UnsplashReposi
 //
 //  internal val searchUsersLiveData: LiveData<List<UserItemModel>> =
 //    generateSearchItemsLiveData<UserItemModel>()
-  
-  private val _searchQuerySubject: BehaviorSubject<String> = BehaviorSubject.createDefault("")
-  private val searchQuerySubject: BehaviorSubject<String> get() = _searchQuerySubject
-  
-  private val _searchPhotosSubject: BehaviorSubject<List<PhotoItemModel>> = BehaviorSubject.create()
-  val searchPhotosSubject: Observable<List<PhotoItemModel>> get() = _searchPhotosSubject.hide()
-  
-  private val _searchUsersSubject: BehaviorSubject<List<UserItemModel>> = BehaviorSubject.create()
-  val searchUsersSubject: Observable<List<UserItemModel>> get() = _searchUsersSubject.hide()
-  
-  private val searchPhotosDisposable: Disposable =
-    generateSearchItemsObservable<PhotoItemModel>()
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(
-        /* onNext = */ { _searchPhotosSubject.onNext(it) },
-        /* onError = */ { Log.d("SearchViewModel", "searchPhotosDisposable:  $it") }
-      )
-  private val searchUsersDisposable: Disposable =
-    generateSearchItemsObservable<UserItemModel>()
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(
-        /* onNext = */ { _searchUsersSubject.onNext(it) },
-        /* onError = */{ Log.d("SearchViewModel", "searchUsersDisposable:  $it") }
-      )
   
   //  // reified is check type at runtime.
 //  private inline fun <reified T> generateSearchItemsLiveData(): LiveData<List<T>> =
@@ -68,11 +41,35 @@ class SearchViewModel @Inject constructor(private val repository: UnsplashReposi
 //        liveData { emit(emptyList()) }
 //      }
 //    }
-//
+  
+  private val _searchQuerySubject: BehaviorSubject<String> = BehaviorSubject.createDefault("")
+  private val searchQuerySubject: BehaviorSubject<String> get() = _searchQuerySubject
+  
+  private val _searchPhotosSubject: BehaviorSubject<List<PhotoItemModel>> = BehaviorSubject.create()
+  val searchPhotosSubject: Observable<List<PhotoItemModel>> get() = _searchPhotosSubject.hide()
+  
+  private val _searchUsersSubject: BehaviorSubject<List<UserItemModel>> = BehaviorSubject.create()
+  val searchUsersSubject: Observable<List<UserItemModel>> get() = _searchUsersSubject.hide()
+  
+  private val disposables = CompositeDisposable()
+  
+  private val searchPhotosDisposable: Disposable =
+    generateSearchItemsObservable<PhotoItemModel>().observeOn(AndroidSchedulers.mainThread())
+      .subscribe(
+        /* onNext = */ { item: List<PhotoItemModel> -> _searchPhotosSubject.onNext(item) },
+        /* onError = */ { throwable: Throwable -> Log.d("SearchViewModel", "searchPhotosDisposable:  ${throwable.message}") }
+      )
+  
+  private val searchUsersDisposable: Disposable =
+    generateSearchItemsObservable<UserItemModel>().observeOn(AndroidSchedulers.mainThread())
+      .subscribe(
+        /* onNext = */ { item: List<UserItemModel> -> _searchUsersSubject.onNext(item) },
+        /* onError = */ { throwable: Throwable -> Log.d("SearchViewModel", "searchUsersDisposable:  ${throwable.message}") }
+      )
+  
   // reified is check type at runtime.
   private inline fun <reified T> generateSearchItemsObservable(): Observable<List<T>> =
-    searchQuerySubject
-      .debounce(650L, TimeUnit.MILLISECONDS)
+    searchQuerySubject.debounce(650L, TimeUnit.MILLISECONDS)
       .distinctUntilChanged()
       .switchMap { query: String -> searchItems<T>(query).toObservable() }
       .onErrorReturn { e: Throwable ->
@@ -84,21 +81,9 @@ class SearchViewModel @Inject constructor(private val repository: UnsplashReposi
   // reified is check type at at runtime.
   private inline fun <reified T> searchItems(query: String): Single<List<T>> = when (T::class) {
     
-    PhotoItemModel::class -> repository
-      .searchPhotos(query, 1, 30)
-      .map { response: SearchPhotosResponse ->
-        response.results.map { result: PhotoItemResponse ->
-          result.toPhotoItemModel()
-        }
-      } as Single<List<T>>
+    PhotoItemModel::class -> repository.searchPhotos(query, 1, 30) as Single<List<T>>
     
-    UserItemModel::class -> repository
-      .searchUsers(query, 1, 30)
-      .map { response: SearchUsersResponse ->
-        response.results.map { result: UserItemResponse ->
-          result.toUserItemModel()
-        }
-      } as Single<List<T>>
+    UserItemModel::class -> repository.searchUsers(query, 1, 30) as Single<List<T>>
     
     else -> throw IllegalArgumentException("Unsupported type")
   }.onErrorReturn { e: Throwable ->
@@ -108,11 +93,11 @@ class SearchViewModel @Inject constructor(private val repository: UnsplashReposi
   
   fun setQuery(query: String) {
     _searchQuerySubject.onNext(query)
+    disposables.addAll(searchPhotosDisposable, searchUsersDisposable)
   }
   
   override fun onCleared() {
-    searchPhotosDisposable.dispose()
-    searchUsersDisposable.dispose()
+    disposables.clear()
     super.onCleared()
   }
 }
