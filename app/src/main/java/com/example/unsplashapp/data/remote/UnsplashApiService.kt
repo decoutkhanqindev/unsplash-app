cf92a6c5bfab1cebce567e48aadd7543288407d7
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-05 00:06:00 +0700

    update interface to add endpoint api UnsplashApiService

--- app/src/main/java/com/example/unsplashapp/data/remote/UnsplashApiService.kt
+++ app/src/main/java/com/example/unsplashapp/data/remote/UnsplashApiService.kt
@@ -5,19 +5,26 @@ import com.example.unsplashapp.data.remote.response.PhotoItemResponse
 import com.example.unsplashapp.data.remote.response.SearchPhotosResponse
 import com.example.unsplashapp.data.remote.response.SearchUsersResponse
 import io.reactivex.rxjava3.core.Single
+import retrofit2.Retrofit
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface UnsplashApiService {
+  
+  companion object {
+    operator fun invoke(retrofit: Retrofit): UnsplashApiService = retrofit.create()
+  }
+  
   @GET("collections")
-  suspend fun getCollections(
+  fun getCollections(
     @Query("page") page: Int, @Query("per_page") perPage: Int
-  ): List<CollectionItemResponse>
+  ): Single<List<CollectionItemResponse>>
   
   @GET("photos")
-  suspend fun getPhotos(
+  fun getPhotos(
     @Query("page") page: Int, @Query("per_page") perPage: Int
-  ): List<PhotoItemResponse>
+  ): Single<List<PhotoItemResponse>>
   
   @GET("search/photos")
   fun searchPhotosByRxJava(
