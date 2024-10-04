46bac6cab6f91c1e3cecb1d2c4ddac9ff3e67d3c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-04 20:31:53 +0700

    update UnsplashGlideModule

--- app/src/main/java/com/example/unsplashapp/di/UnsplashGlideModule.kt
+++ app/src/main/java/com/example/unsplashapp/di/UnsplashGlideModule.kt
@@ -7,20 +7,35 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
+import dagger.hilt.EntryPoint
+import dagger.hilt.EntryPoints
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import java.io.InputStream
-import javax.inject.Inject
 
 @GlideModule
 class UnsplashGlideModule : AppGlideModule() {
   
-  @Inject
-  internal lateinit var okHttpClient: OkHttpClient
-  
   override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
-    super.registerComponents(context, glide, registry)
+    // get the entry point for the OkHttp client
+    val entryPoint: OkHttpEntryPoint = EntryPoints.get(context, OkHttpEntryPoint::class.java)
+    
+    // get OkHttpClient from the entry point
+    val okHttpClient: OkHttpClient = entryPoint.provideOkHttpClient()
+    
     registry.replace(
-      GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(okHttpClient)
+      GlideUrl::class.java,
+      InputStream::class.java,
+      OkHttpUrlLoader.Factory(okHttpClient)
     )
+  }
+  
+  // @EntryPoint annotation in Hilt is used to define an interface that serves as an entry point
+  // to access dependencies provided by Hilt in classes that Hilt doesn't manage directly.
+  @EntryPoint
+  @InstallIn(SingletonComponent::class)
+  internal interface OkHttpEntryPoint {
+    fun provideOkHttpClient(): OkHttpClient
   }
 }
